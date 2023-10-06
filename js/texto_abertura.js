49d096537eff72df243b02e96f1cd04a1b0799da
Tainah91 <tainahaki10@gmail.com>
2023-10-05 21:20:37 -0300

    finalizado com javascript

---
+++ js/texto_abertura.js
@@ -0,0 +1,20 @@
+function inicio() {
+    /* let- ou um ou outro, var-varios(variante) e const- recursos fixos*/
+    const textoOriginal = "Planeta Alien";
+    const elementoTexto = document.querySelector("p")
+    const audio = document.querySelector("audio")
+    const btn = document.querySelector("button")
+    const atrasoRapido = 5000 // Atraso em milissegundos (2 segundos)
+    const atrasoDevagar = 7000
+
+    setTimeout(function () {
+        btn.style.display = "none"
+        audio.play()
+    }, atrasoRapido);
+
+    setTimeout(function () {
+        elementoTexto.textContent = textoOriginal
+    }, atrasoDevagar);
+
+
+}
